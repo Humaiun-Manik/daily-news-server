f702a370ab59e75a7994e971634f45699f897c98
humaiun <humaiunkobirr@gmail.com>
2023-06-15 23:46:56 +0600

    create category & news api

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require("./categories.json");
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Daily news server is running");
@@ -14,6 +15,22 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
